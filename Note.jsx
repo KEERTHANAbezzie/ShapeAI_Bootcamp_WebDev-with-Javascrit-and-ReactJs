2b9c10e28b269143fc9fc3c103bf763b1f412acf
KEERTHANAbezzie <85439834+KEERTHANAbezzie@users.noreply.github.com>
2021-06-07 13:32:47 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>React Js and Javascript</h1>
+      <p>
+        This is a bootcamp conducted by Shape AI for seven days. This is an
+        amazing startup for beginers.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
